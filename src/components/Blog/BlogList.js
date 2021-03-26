92fc5aecd6aaa7f6e35f5a7c18c4c99f9296a31e
Shubham Kumar Jha <kumarshubhamjha2157@gmail.com>
2021-03-26 21:16:49 +0530

    added axios and setup blog list

--- src/components/Blog/BlogList.js
+++ src/components/Blog/BlogList.js
@@ -1,23 +1,41 @@
 import React, { Component } from "react";
 import img1 from "../../Assets/home-bg.jpg"
 import Card from "./BlogCard";
+import axios from "axios";
 
+
+
+//get();
 const Blogs = props => {
+    const [Blog, setBlog] = React.useState([]);
+    const get = () =>{
+        axios.get("https://blog-api-v01.herokuapp.com/blogs/")
+        .then(response =>{
+            // console.log(response.data);
+            setBlog(response.data)
+        })
+
+    } 
+    React.useEffect(() => {
+        get(); 
+    },[])  
+    
         return (
             <div className="container-fluid d-flex justify-content-center">
                 <div className="row">
-                    <div className="col-md-4">
-                        <Card imgsrc={img1} title="Card Title 1" content="Paragraph 1 Lorem ipsum dolor sit amet, connect. Lorem ips. Lorem ipsum dolor. Lorem ipsum
+                    
+                   
+                    {Blog.map((item, index) =>{
+                          console.log(item.cover_image.url);
+                          return(
+                            <div className="col-md-3">
+                        <Card imgsrc={item.cover_image.url} title={item.title} content="Paragraph 1 Lorem ipsum dolor sit amet, connect. Lorem ips. Lorem ipsum dolor. Lorem ipsum
                     .lorum dolor. Lorem ipsum dolor. Lorem" />
                     </div>
-                    <div className="col-md-4">
-                        <Card imgsrc={img1} title="Card Title 2" content="Paragraph 2 Lorem ipsum dolor sit amet, connect. Lorem ips. Lorem ipsum dolor. Lorem ipsum
-                    .lorum dolor. Lorem ipsum dolor. Lorem" />
-                    </div>
-                    <div className="col-md-4">
-                        <Card imgsrc={img1} title="Card Title 3" content="Paragraph 3 Lorem ipsum dolor sit amet, connect. Lorem ips. Lorem ipsum dolor. Lorem ipsum
-                    .lorum dolor. Lorem ipsum dolor. Lorem" />
-                    </div>
+                          )
+                }
+                )}
+                    
                 </div>
             </div>
         )
