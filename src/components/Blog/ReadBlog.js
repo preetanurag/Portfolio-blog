c7352e9d4a90c0136893fbb2cdef25ddb5c8117d
Preet Anurag <preetanurag5@gmail.com>
2021-03-27 12:26:44 +0530

    handled timeout

--- src/components/Blog/ReadBlog.js
+++ src/components/Blog/ReadBlog.js
@@ -5,19 +5,31 @@ import "./read-blog.css";
 import ReactMarkdown from 'react-markdown'
 
 
+
+
 const ReadBlog = props => {
 
     const [Blog, setBlog] = React.useState([]);
+    const [isloaded,setisloaded] = React.useState(false);
+
+   
     const get = () =>{
         axios.get("https://blog-api-v01.herokuapp.com/blogs/60426b1a08a6e46c81c37f36")
         .then(response =>{
              console.log(response.data);
             setBlog(response.data)
+            setisloaded(true);
         })
 
     } 
+
+    const handletime = () =>{
+        setTimeout(()=>{
+            get();
+        },3000)
+    }
     React.useEffect(() => {
-        get(); 
+        handletime(); 
     },[])  
     
         return (
@@ -31,8 +43,13 @@ const ReadBlog = props => {
                     </div>
                 </div>
 
+               {isloaded==false ?
+               <img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg" alt="cover" className="img-thumbnail img-fluid" /> :
+
+                   <img src={Blog.cover_image.url} alt="cover" className="img-thumbnail img-fluid" /> 
+
+               }
                 
-                {/* <img src={Blog.cover_image.url} alt="cover" className="img-thumbnail" />  */}
                     
                 <ReactMarkdown>{Blog.content}</ReactMarkdown>
                 
