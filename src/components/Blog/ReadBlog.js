c67671b8e9fd29e1d350083590208eb37813cb1c
Preet Anurag <preetanurag5@gmail.com>
2021-04-14 13:06:38 +0530

    changed readblog

--- src/components/Blog/ReadBlog.js
+++ src/components/Blog/ReadBlog.js
@@ -51,7 +51,7 @@ const ReadBlog = props => {
                 <div className="jumbotron jumbotron-fluid">
                     <div className="container">
                         <h1 className="display-4">{Blog.title}</h1>
-                        <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                       
                     </div>
                 </div>
 
