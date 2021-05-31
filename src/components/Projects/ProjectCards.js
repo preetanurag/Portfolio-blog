c690378107a5089d7d84bf2edee62b662aa5b4a0
Satyamkrjha <skjha2646@gmail.com>
2021-05-31 23:56:29 +0530

    ui enhanced

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,24 +1,23 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import "../../style.css";
+import "./projectstyle.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function ProjectCards(props) {
   return (
-    <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "center"}}>
-          {props.description}
-        </Card.Text>
+    <div className="card1 text-center shadow">
+      <div className="overflow">
+        <img src={props.imgPath} alt="image1" className="card-img-top" />
+      </div>
+      <div className="card-body text-dark">
+        <h4 className="card-title">{props.title}</h4>
+        <p className="card-text text-secondary">{props.description}</p>
         <Button variant="primary" href={props.link} target="_blank">
           <i className="cil-external-link">&nbsp;</i>
           {props.isBlog ? "View Blog" : "View Project"}
         </Button>
-      </Card.Body>
-    </Card>
+      </div>
+    </div>
   );
 }
 export default ProjectCards;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -28,52 +28,61 @@ React.useEffect(() => {
 
 
   return (
-    <Container fluid className="project-section">
-      <Particle/>
-      <Container>
+    <div>
+      <div className="main-head">
+        <Particle />
         <h1 className="project-heading">
-          My <strong className="purple">Projects </strong>
+          My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Visit my some of the projects
+          Here are a few projects I've worked on recently.
         </p>
-        {t ?
-        
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-          {Project.map((item, index) =>{
-                          
-                          return(
-            <ProjectCard
-              imgPath={item.cover.url}
-              isBlog={false}
-              title={item.title}
-              description={item.description}
-              link={item.link}
-            />
-             )
-                          })}
-          </Col>
-        </Row>
-       :(
-
-        <div className="container-fluid d-flex justify-content-center">  
-        <div style={{ width: 150, height: 150, padding:20}}>
-        <ChangingProgressProvider values={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}>
-    {percentage => (
-      <CircularProgressbar value={percentage}  styles={buildStyles({
-        pathColor: "rgb(23, 23, 23)",
-        trailColor: "orange"
-      })}/>
-    )}
-  </ChangingProgressProvider>
-        </div>
-        </div>
-       )
-
-    }
-      </Container>
-    </Container>
+        {t ? (
+          <div className="container-fluid d-flex justify-content-center">
+            <div className="row">
+              {Project.map((item, index) => {
+                return (
+                  <div className="col-md-4">
+                    <ProjectCard
+                      imgPath={item.cover.url}
+                      isBlog={false}
+                      title={item.title}
+                      description={item.description}
+                      link={item.link}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        ) : (
+          <div className="container-fluid d-flex justify-content-center">
+            <div
+              style={{
+                width: 150,
+                height: 150,
+                padding: 20,
+                justifyContent: "center",
+              }}
+            >
+              <ChangingProgressProvider
+                values={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+              >
+                {(percentage) => (
+                  <CircularProgressbar
+                    value={percentage}
+                    styles={buildStyles({
+                      pathColor: "rgb(23, 23, 23)",
+                      trailColor: "orange"
+                    })}
+                  />
+                )}
+              </ChangingProgressProvider>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
